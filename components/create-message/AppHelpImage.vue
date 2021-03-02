f619b4bcdbf0bbfda5d64e89f49cbc5867a51666
tarick-kr <tsg-kr@ukr.net>
2021-03-02 19:31:03 +0200

    add tooltip

---
+++ components/create-message/AppHelpImage.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-expansion-panels flat>
+    <v-expansion-panel>
+      <v-expansion-panel-header class="px-0 pt-1 pb-2">
+        Каким должно быть изображение и как его подготовить?
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <p>
+          Изображение должно быть ближе к квадратному и в формате jpeg/jpg. Также поддерживается формат png
+          однако его лучше не использовать из-за большего размера файла.
+          Перед загрузкой файл следует оптимизировать для вэба (уменьшить его размер без потери качества),
+          например воспользовавшись <a href="https://tinypng.com/">этим</a> сайтом.
+        </p>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+
+<script>
+export default {
+  name: 'AppHelpImage'
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/create-message.vue
+++ pages/create-message.vue
@@ -2,6 +2,7 @@
   <div class="create-message-page wrapper-main">
     <v-container
       id="scrollWrapper"
+      v-resize.quiet="onResize"
       class="overflow-y-auto"
       fluid
     >
@@ -12,13 +13,42 @@
         <div
           v-scroll:#scrollWrapper="onScroll"
         >
+          <div class="pt-3 pb-3 title-page">
+            <h2>Создание рассылки</h2>
+          </div>
+          <v-divider
+            dark
+            class="title-page"
+          />
           <form
             class="form-admins"
             @submit.prevent="submit"
           >
-            <h2 class="subtitle-1 mt-4 font-weight-bold">
-              Укажите email получателя:
-            </h2>
+            <div class="d-flex">
+              <h2 class="subtitle-1 font-weight-bold">
+                Укажите адреса получателей:
+              </h2>
+              <v-tooltip
+                :right="windowWidth >= 760"
+                :top="windowWidth < 760"
+                attach="#scrollWrapper"
+                content-class="tooltip-style"
+              >
+                <template #activator="{ on, attrs }">
+                  <v-icon
+                    class="tooltip-icon"
+                    medium
+                    dark
+                    right
+                    v-bind="attrs"
+                    v-on="on"
+                  >
+                    mdi-information-outline
+                  </v-icon>
+                </template>
+                <span>Email адреса следует вводить через запятую</span>
+              </v-tooltip>
+            </div>
             <v-text-field
               v-model="email"
               dark
@@ -100,6 +130,7 @@ import { validationMixin } from 'vuelidate';
 import { required, email } from 'vuelidate/lib/validators';
 import { API_KEY_TINYMCE } from '@/server/keys';
 import Editor from '@tinymce/tinymce-vue';
+// import AppHelpImage from '@/components/create-message/AppHelpImage';
 
 export default {
   name: 'CreateMessagePage',
@@ -114,6 +145,7 @@ export default {
       email: '',
       subject: '',
       loading: false,
+      windowWidth: 0,
     };
   },
   validations: {
@@ -162,6 +194,9 @@ export default {
     this.$store.commit('OFFSET_TOP', 0);
     console.log('OFFSET_TOP - ', this.$store.getters.offsetTop);
   },
+  mounted() {
+    this.windowWidth = window.innerWidth;
+  },
   methods: {
     onScroll(e) {
       console.log('onScroll');
@@ -186,6 +221,9 @@ export default {
         }
       }
     },
+    onResize() {
+      this.windowWidth = window.innerWidth;
+    },
   },
 };
 </script>
@@ -195,11 +233,14 @@ export default {
   .wrapper-main
     #scrollWrapper
       max-height: calc(100vh - 64px)
+      .title-page
+        margin-bottom: 16px
+        margin-left: 25px
+        max-width: 800px
       .form-admins
         width: fit-content
         min-width: 800px
         margin-left: 25px
-
         .error-description-hint
           font-size: 12px
           color: #ff5252
@@ -214,6 +255,18 @@ export default {
           margin-top: 24px
           .btn-action
             margin-right: -16px
+
+  // Стили для всплывающей подсказки
+  .wrapper-main
+    #scrollWrapper
+      .tooltip-style
+        color: #FFAD00
+        border: 1px solid #FFAD00
+        background-color: #224955
+      .form-admins
+        .tooltip-icon
+          &:hover
+            color: #FFAD00
 
   // Скрытие скролла
   .wrapper-main
@@ -319,12 +372,16 @@ export default {
   @media screen and (max-width: 960px)
     .wrapper-main
       #scrollWrapper
+        .title-page
+          max-width: 600px
         .form-admins
           min-width: 600px
 
   @media screen and (max-width: 760px)
     .wrapper-main
       #scrollWrapper
+        .title-page
+          margin-right: 25px
         .form-admins
           min-width: unset
 
@@ -332,10 +389,43 @@ export default {
     .wrapper-main
       #scrollWrapper
         max-height: calc(100vh - 64px)
+        .title-page
+          margin-right: 20px
+          margin-left: 20px
         .form-admins
           margin-left: 20px
 
-  @media screen and (max-width: 450px)
+  @media screen and (max-width: 500px)
+    .wrapper-main
+      #scrollWrapper
+        .title-page
+          margin: 0 auto
+
+  @media screen and (max-width: 480px)
+    .wrapper-main
+      #scrollWrapper
+        .form-admins
+          margin: 0 auto
+          ::v-deep.tox.tox-tinymce.tox-platform-touch
+            max-width: 406px
+
+  @media screen and (max-width: 460px)
+    .wrapper-main
+      #scrollWrapper
+        .form-admins
+          margin: 0 auto
+          ::v-deep.tox.tox-tinymce.tox-platform-touch
+            max-width: 370px
+
+  @media screen and (max-width: 400px)
+    .wrapper-main
+      #scrollWrapper
+        .form-admins
+          margin: 0 auto
+          ::v-deep.tox.tox-tinymce.tox-platform-touch
+            max-width: 336px
+
+  @media screen and (max-width: 380px)
     .wrapper-main
       #scrollWrapper
         .form-admins
