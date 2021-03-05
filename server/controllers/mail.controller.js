8b1d23188dfcc750b5bfac615a26498831dc260f
tarick-kr <tsg-kr@ukr.net>
2021-03-05 10:44:05 +0200

    add template mail

--- server/controllers/mail.controller.js
+++ server/controllers/mail.controller.js
@@ -21,18 +21,46 @@ module.exports.sendMessage = async (req, res) => {
     user.messages.push(message._id);
     await user.save();
 
+    const template = `
+    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+    <html>
+      <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+        <title>S.TAR.MAIL</title>
+        <style type="text/css">
+        </style>
+      </head>
+      <body style="margin:10; padding:10;">
+        <table cellspacing="0" cellpadding="0" style="background:#224955;width:760px;font-family:arial;color:#fff;border-spacing:0;border-collapse:collapse;border:1px solid #ccc;margin-left: auto; margin-right:auto;">
+          <tr>
+            <td>
+              <table cellspacing="0" cellpadding="0" style="background:#224955;width:100%;border-spacing: 0;border-collapse:collapse;">
+                <tr>
+                  <td style="padding: 20px 50px 20px 50px;">
+                    <img src="https://s-tar-mail.herokuapp.com/app_logo_0.5.png" alt="app_logo" width="145" height="30" />
+                  </td>
+                </tr>
+              </table>
+              <hr style="border-top: none;margin: 0;border-color: #FFAD00">
+              <table cellspacing="0" cellpadding="0" style="background:#224955;width:100%;border-spacing: 0;border-collapse:collapse;">
+                <tr>
+                  <td style="padding:50px 55px 50px 50px;">
+                    ${req.body.textMail}
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </body>
+    </html>
+    `;
+
     const mail = {
       from: `S.TAR.MAIL <${keys.EMAIL}>`,
       to: req.body.emailsArray,
       subject: req.body.subject,
-      html: `
-          ${req.body.textMail}
-          <br>
-          <br>
-          <br>
-          <hr>
-          <br>
-          <p>Данное письмо не требует ответа.<p>`,
+      html: template,
     };
     await sendEmail(mail);
 
